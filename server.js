c8599a3a5a6c8ca2ac7b813798c7da7c7a14829f
Abdelkerim Habib Doutoum <mahamathabibabdelkerimabdelker@gmail.com>
2026-07-11 20:16:36 +0400

    Create basic Express Backend Server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from my backend!"
+    });
+});
+
+
+app.get("/about", (req, res) => {
+    res.json({
+        name: "Abdelkerim",
+        course: "AI & Data Science"
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
