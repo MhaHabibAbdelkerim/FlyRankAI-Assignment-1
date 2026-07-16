38b0a7279d307887be17f0ff7efea7ea84671820
Abdelkerim Habib Doutoum <mahamathabibabdelkerimabdelker@gmail.com>
2026-07-16 08:24:48 +0400

    CRUD

--- server.js
+++ server.js
@@ -79,6 +79,50 @@ app.post("/tasks", (req, res) => {
 
 });
 
+app.put("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    const { title, done } = req.body;
+
+    if (
+        (title !== undefined && title.trim() === "") ||
+        (done !== undefined && typeof done !== "boolean")
+    ) {
+        return res.status(400).json({
+            error: "Invalid input"
+        });
+    }
+
+    if (title !== undefined) task.title = title;
+    if (done !== undefined) task.done = done;
+
+    res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const index = tasks.findIndex(task => task.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(index, 1);
+
+    res.status(204).send();
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
